5d1f469011705a04003eec94797aca55aaea9383
Yusuke Tanaka <y.tanaka@aktsk.jp>
2015-01-08 19:49:41 +0900

    default['sqlcipher']['git_user'] = 'root'

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['sqlcipher']['repository'] = 'https://github.com/sqlcipher/sqlcipher.git'
 default['sqlcipher']['version'] = 'v3.2.0'
-default['sqlcipher']['git_user'] = "deployer"
+default['sqlcipher']['git_user'] = 'root'
 
 default['sqlcipher']['working_dir'] = "#{Chef::Config[:file_cache_path]}/sqlcipher"
